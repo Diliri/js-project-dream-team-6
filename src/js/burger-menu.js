fed9501c5076050425e0b5941390f7cff064ba4d
Diana Muzhylivska <diana.listen@gmail.com>
2026-06-18 16:44:23 +0300

    burger-menu відкат до +- робочого стану

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,23 +1,23 @@
 // Связываем кнопку напарницы с вашим бургер-меню
-const openMenuBtn = document.querySelector('#menu-open');       // Кнопка из хедера 
+const openMenuBtn = document.querySelector('#menu-open');       // Кнопка из хедера напарницы
 const closeMenuBtn = document.querySelector('#closeMenuBtn');   // Ваш крестик
 const mobileMenu = document.querySelector('#burgerMenu');       // Ваше меню
 const menuLinks = document.querySelectorAll('.burger-menu__link, .burger-menu__btn');   
 
 if (openMenuBtn && closeMenuBtn && mobileMenu) {
-  // Відкрити меню
+  // Открыть ваше меню
   openMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.add('is-open');
     document.body.style.overflow = 'hidden';
   });
 
-  // Закрити меню
+  // Закрыть ваше меню
   closeMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('is-open');
     document.body.style.overflow = '';
   });
 
-  // Закрити при кліку на посилання
+  // Закрыть при клике на ссылки
   menuLinks.forEach(link => {
     link.addEventListener('click', () => {
       mobileMenu.classList.remove('is-open');
