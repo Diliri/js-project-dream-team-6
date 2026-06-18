0cce769b99c2c3e6a9aa214fd08e2bd2f7925f72
tyoska <tyoska111@ukr.net>
2026-06-18 15:38:31 +0300

    FAQ some fix

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,6 @@
 async function initAccordion() {
   const { default: Accordion } = await import('accordion-js');
-  await import('accordion-js/dist/accordion.min.css');
+  // await import('accordion-js/dist/accordion.min.css'); - переніс імпорт в цсс що б не перебивало стилі. =)
 
   new Accordion('.accordion-container', {
     duration: 400,
